d48c2f3ddd84698acf3fdabd23e438edb236320a
Alicja Slupska <ala.slupska@gmail.com>
2021-02-04 16:31:54 +0100

    file upload: backend setup

--- client/src/components/plants/AddPlant.js
+++ client/src/components/plants/AddPlant.js
@@ -29,6 +29,7 @@ class AddPlant extends React.Component {
     return (
       <div>
         <h2>Add a new plant</h2>
+        {/* <form enctype="multipart/form-data"> */}
         <form>
           <input type="text" name="name" placeholder="Plant name" value={this.state.name} onChange={this.handleChangeInput} className="form-control input-field"></input>
           <textarea
@@ -38,7 +39,9 @@ class AddPlant extends React.Component {
             onChange={this.handleChangeInput}
             className="form-control input-field"
           ></textarea>
-          {/* <input type="file" name="plantImg"></input> */}
+          <label>Upload an image
+          <input type="file" name="plantImg"></input>  
+          </label>
           <button onClick={this.submitHandler} className="btn btn-primary">
             Save Plant
           </button>
