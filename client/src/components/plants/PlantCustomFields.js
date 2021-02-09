f9816160fda165bf7cd8d2e788e8e84ee3526bdc
tmat <tamao.matsukawa@gmail.com>
2021-02-09 19:02:29 +0100

    new component added to search and details

--- client/src/components/plants/PlantCustomFields.js
+++ client/src/components/plants/PlantCustomFields.js
@@ -6,13 +6,22 @@ class PlantCustomFields extends Component {
   // this.props.dataFromUser to match in PlantDetails
 
   render() {
-    console.log("THIS.PROPS", this.props);
+    // console.log("THIS.PROPS", this.props);
 
     return (
       <div className="plantCustomFields">
         <h3> Name: {this.props.dataFromUser.name}</h3>
         <p> Your Notes: {this.props.dataFromUser.notes}</p>
-        <p> Your plant picture: </p>
+        <p>
+          {" "}
+          Your plant picture:{" "}
+          <img
+            src={this.props.dataFromUser.plantImg || "/images/growing.png"}
+            width="180"
+            height="180"
+            alt="defaultPlantImg"
+          />
+        </p>
       </div>
     );
   }
--- client/src/components/trefle-search/TrefleSearchDetails.js
+++ client/src/components/trefle-search/TrefleSearchDetails.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import TrefleInfoPlantList from "../plants/TrefleInfoPlantList";
 export default class TrefleSearchDetails extends Component {
   state = {
     plant: null,
@@ -11,7 +12,8 @@ export default class TrefleSearchDetails extends Component {
     const { params } = this.props.match;
 
     axios.get(`/api/search/detail/${params.slug}`).then((responseFromApi) => {
-      console.log(responseFromApi);
+      console.log("CHECK THE RESPONSE!!!", responseFromApi);
+
       this.setState({
         plant: responseFromApi.data.data,
         loading: false,
@@ -20,148 +22,20 @@ export default class TrefleSearchDetails extends Component {
   }
 
   render() {
+    console.log("CHECK THE STATE!", this.state);
+    if (this.state.loading) {
+      return <h1>Loading...</h1>;
+    }
     return (
       <div className="treflePlantDetails">
-        {this.state.loading && <h1>Loading...</h1>}
+        {/* {this.state.plant.scientific_name} */}
 
-        {this.state.plant && (
-          <div>
-            <img
-              className="apiPlantImage"
-              src={this.state.plant.image_url}
-              width="180"
-              height="180"
-              alt="selected plant"
-            />
-            <div className="commonName">
-              {" "}
-              <h3>Names:</h3>
-              <h4>Common name:</h4>
-              {this.state.plant.common_name == null && (
-                <p>information unavailable</p>
-              )}{" "}
-              <p style={{ textTransform: "capitalize" }}>
-                {this.state.plant.common_name}{" "}
-              </p>
-              <h4> Scientific name:</h4>
-              {this.state.plant.scientific_name == null && (
-                <p>information unavailable</p>
-              )}{" "}
-              <p> {this.state.plant.scientific_name}</p>{" "}
-              <h4>Family common name:</h4>
-              {this.state.plant.family.common_name == null && (
-                <p>information unavailable</p>
-              )}{" "}
-              <p> {this.state.plant.family.common_name}</p>{" "}
-            </div>
-            <h3>Gallery: </h3>
-            <img
-              className="apiPlantImage"
-              src={this.state.plant.main_species.images.flower[0].image_url}
-              width="180"
-              height="180"
-              alt="imgFlower"
-            />
-            <img
-              className="apiPlantImage"
-              src={this.state.plant.main_species.images.flower[1].image_url}
-              width="180"
-              height="180"
-              alt="imgFlower1"
-            />
-            <img
-              className="apiPlantImage"
-              src={this.state.plant.main_species.images.leaf[0].image_url}
-              width="180"
-              height="180"
-              alt="imgLeaf"
-            />
-            <h3>Distribution Native Countries:</h3>
-            {this.state.plant.main_species.distribution.native
-              .slice(0, 2)
-              .map((item, index) => {
-                return <div key={index}>{item} </div>;
-              })}
-            <div className="plantGrowth">
-              {" "}
-              <h3>Growth:</h3>
-              <h4>PH Maximum:</h4>
-              {this.state.plant.main_species.growth.ph_maximum == null && (
-                <p>information unavailable </p>
-              )}{" "}
-              <p>{this.state.plant.main_species.growth.ph_maximum}</p>
-              <h4>PH Minimum:</h4>
-              {this.state.plant.main_species.growth.ph_minimum == null && (
-                <p>information unavailable</p>
-              )}{" "}
-              <p>{this.state.plant.main_species.growth.ph_minimum}</p>
-              <h4>Light*:</h4>
-              {this.state.plant.main_species.growth.light == null && (
-                <p>information unavailable</p>
-              )}{" "}
-              <p>{this.state.plant.main_species.growth.light}</p>
-              <h4>Bloom months:</h4>
-              {this.state.plant.main_species.growth.bloom_months == null && (
-                <p>information unavailable</p>
-              )}{" "}
-              <p>{this.state.plant.main_species.growth.bloom_months}</p>
-              <h4>Atmospheric humidity**:</h4>
-              {this.state.plant.main_species.growth.atmospheric_humidity ==
-                null && <p>information unavailable</p>}
-              <p>{this.state.plant.main_species.growth.atmospheric_humidity}</p>
-              <h4>Minimum temperature:</h4>
-              {this.state.plant.main_species.growth.minimum_temperature.deg_c ==
-                null && <p>information unavailable</p>}
-              <p>
-                {this.state.plant.main_species.growth.minimum_temperature.deg_c}
-                C°
-              </p>
-              <h4>Maximum temperature:</h4>
-              {this.state.plant.main_species.growth.maximum_temperature.deg_c ==
-                null && <p>information unavailable</p>}{" "}
-              <p>
-                {this.state.plant.main_species.growth.maximum_temperature.deg_c}
-                C°
-              </p>
-              <h4>Soil texture:</h4>
-              {this.state.plant.main_species.growth.soil_texture == null && (
-                <p>information unavailable</p>
-              )}{" "}
-              <p>{this.state.plant.main_species.growth.soil_texture}</p>
-              <h4>Soil humidity:</h4>
-              {this.state.plant.main_species.growth.soil_humidity == null && (
-                <p>information unavailable</p>
-              )}{" "}
-              <p>{this.state.plant.main_species.growth.soil_humidity}</p>
-              <p>
-                * Light: Required amount of light, on a scale from 0 (no light,
-                less than/equal to 10 lux) to 10 (very intensive insolation,
-                reater than/equal to100 000 lux) <br></br>
-                **Atmospheric humidity: Required relative humidity in the air,
-                on a scale from 0 (less than/equal to 10%) to 10 (greater
-                than/equal to 90%)
-              </p>
-            </div>
-            <Link to={`/add-plant/${this.state.plant.slug}`}>
-              Add to My Plant Collection
-            </Link>
-          </div>
-        )}
+        <TrefleInfoPlantList trefleInfoPlant={this.state.plant} />
+
+        <Link to={`/add-plant/${this.state.plant.slug}`}>
+          Add to My Plant Collection
+        </Link>
       </div>
     );
   }
 }
-
-// common_name
-// scientific_name
-// family_common_name
-// images [could be an additional gallery]
-// distribution.native [could be 2-3 places] - shall we list them all since they are in alphabetical order= or display them randomly?
-// growth.ph_maxiumum
-// growth.ph_minimum
-// growth.light
-// growth.atmospheric_humidity
-// growth.minimum_temperature
-// growth.maximum_temperature
-// growth.soil_texture
-// growth.soil_humidity
