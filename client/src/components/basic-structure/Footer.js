b086fd8753ec68cd71b8856e0e6135aa6e5a93fc
Alicja Slupska <ala.slupska@gmail.com>
2021-02-24 19:33:04 +0100

    Shutterstock, Flaticon disclaimer added, flaticon changed

--- client/src/components/basic-structure/Footer.js
+++ client/src/components/basic-structure/Footer.js
@@ -4,9 +4,13 @@ import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <footer className="mt-2" >
-      <p>Created with <img src="/images/plant-heart.png" alt="plant inside of a small heart" /> by Tamao and Alicja </p>
-      <p>Would you like to see more? To check the code visit our <Link to='https://github.com/GitAlicja/plant-parenthood' alt='project-remote-repository' className="green-link">GitHub repository</Link>
+    <footer className="mt-3 mb-2" >
+      <p className="px-4">Created with <img src="/images/plant-heart.png" alt="plant inside of a small heart" /> by Tamao and Alicja
+      <br />
+      To see more visit our <Link to='https://github.com/GitAlicja/plant-parenthood' className="green-link">GitHub repository</Link>
+      <br />
+      The source code is <Link to="https://opensource.org/licenses/mit-license.php" className="green-link">MIT</Link> licensed.
+      The website content uses <Link to="https://www.shutterstock.com/" className="green-link">Shutterstock</Link> graphic material and icons made by <Link to="https://www.freepik.com" className="green-link">Freepik</Link> from <Link to="https://www.flaticon.com/" className="green-link">www.flaticon.com</Link>
       </p>
     </footer>
   );
